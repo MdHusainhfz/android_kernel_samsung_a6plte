61ed0e16986834ae223fb1c3ff4a2681a3107f2e
Dan Williams <dan.j.williams@intel.com>
2019-04-18 17:50:44 -0700

    init: initialize jump labels before command line option parsing

--- init/main.c
+++ init/main.c
@@ -603,26 +603,8 @@ asmlinkage __visible void __init start_kernel(void)
 
 #ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
 	pr_notice("Kernel command line: %s\n", boot_command_line);
-#else
-	for (i = 0; i < ARRAY_SIZE(erase_string); i++) {
-		len = strlen(erase_string[i]);
-		erase_cmd_start = strstr(erased_command_line, erase_string[i]);
-		if (erase_cmd_start == NULL)
-			continue;
-
-		erase_cmd_end = strstr(erase_cmd_start, " ");
-
-		if ( (erase_cmd_end != NULL) && (erase_cmd_start != NULL)
-				&& (erase_cmd_end > erase_cmd_start) ) {
-			value_len = (size_t)(erase_cmd_end - erase_cmd_start) - len;
-
-			for (j = 0; j < value_len; j++) {
-				erase_cmd_start[len + j] = '0';
-			}
-		}
-	}
-	pr_notice("Kernel command line: %s\n", erased_command_line);
-#endif
+	/* parameters may set static keys */
+	jump_label_init();
 	parse_early_param();
 	after_dashes = parse_args("Booting kernel",
 				  static_command_line, __start___param,
@@ -631,8 +613,6 @@ asmlinkage __visible void __init start_kernel(void)
 	if (!IS_ERR_OR_NULL(after_dashes))
 		parse_args("Setting init args", after_dashes, NULL, 0, -1, -1,
 			   NULL, set_init_arg);
-
-	jump_label_init();
 
 	/*
 	 * These use large bootmem allocations and must precede
